befc76e2bf5e9291eb0968aeb035d67e89faabbf
Hu6r1s <darmaa333@gmail.com>
2023-06-23 12:07:08 +0900

    [Fix] Oauth error

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,27 +1,27 @@
-import bcrypt from 'bcrypt';
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 
-import prisma from '@/app/libs/prismadb';
-import { NextResponse } from 'next/server';
+import prisma from "@/app/libs/prismadb";
 
 export async function POST(
-  request: Request
+  request: Request,
 ) {
-  const body = await request.json()
+  const body = await request.json();
   const {
     email,
     name,
-    password
-  } = body
+    password,
+  } = body;
 
-  const hashedPassword = await bcrypt.hash(password, 12)
+  const hashedPassword = await bcrypt.hash(password, 12);
 
   const user = await prisma.user.create({
     data: {
       email,
       name,
-      hashedPassword
+      hashedPassword,
     }
-  })
+  });
 
   return NextResponse.json(user);
 }
