e4cea2fa09d0da788c773d4f70c59d997d36a468
Hu6r1s <darmaa333@gmail.com>
2023-07-10 16:51:35 +0900

    Listing creation

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,6 +2,7 @@
 
 import useLoginModal from '@/app/hooks/useLoginModal';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useRentModal from '@/app/hooks/useRentModal';
 import { SafeUser } from '@/app/types';
 import { User } from '@prisma/client';
 import { signOut } from 'next-auth/react';
@@ -19,16 +20,25 @@ const UserMenu: React.FC<UserMenuProps> = ({
 }) => {
   const registerModal = useRegisterModal()
   const loginModal = useLoginModal()
+  const rentModal = useRentModal()
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value)
   }, [])
+
+  const onRent = useCallback(() => {
+    if (!currentUser) {
+      loginModal.onOpen()
+    }
+
+    rentModal.onOpen()
+  }, [currentUser, loginModal, rentModal])
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div
-          onClick={() => { }}
+          onClick={onRent}
           className="
             hidden
             md:block
@@ -104,7 +114,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                   label="My properties"
                 />
                 <MenuItem
-                  onClick={() => { }}
+                  onClick={rentModal.onOpen}
                   label="Airbnb my home"
                 />
                 <MenuItem
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from 'react-hot-toast'
 import getCurrentUser from './actions/getCurrentUser'
 import LoginModal from './components/modals/LoginModal'
 import RegisterModal from './components/modals/RegisterModal'
+import RentModal from './components/modals/RentModal'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 
@@ -26,10 +27,13 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <Toaster />
+        <RentModal />
         <LoginModal />
         <RegisterModal />
         <Navbar currentUser={currentUser} />
-        {children}
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
       </body>
     </html>
   )
